2026730cba731f9a04de83c87bedd575d5aa95c4
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-07-12 22:26:18 +0600

    catch async function created

--- src/modules/mango/mango.controller.ts
+++ src/modules/mango/mango.controller.ts
@@ -1,46 +1,50 @@
 import { Request, Response } from "express";
 import Mango from "./mango.model";
 import { MangoServices } from "./mango.service";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createMango = async (req: Request, res: Response) => {
-  try {
-    const data = await MangoServices.createMangoIntoDB(req.body)
+// const createMango = async (req: Request, res: Response) => {
+//   try {
+//     const data = await MangoServices.createMangoIntoDB(req.body)
 
-    res.send({
-      success: true,
-      message: "Mango Created Successfully",
-      data,
-    });
-  } catch (error) {
-    res.send({
-      success: false,
-      message: "Error Hppend",
-      error,
-    });
-  }
-};
+//     res.send({
+//       success: true,
+//       message: "Mango Created Successfully",
+//       data,
+//     });
+//   } catch (error) {
+//     res.send({
+//       success: false,
+//       message: "Error Hppend",
+//       error,
+//     });
+//   }
+// };
 
-const getMangos = async (req: Request, res: Response) => {
-  try {
-    const data = await Mango.find();
-    res.send({
-      success: true,
-      message: "Mango getting Successfully",
-      data,
-    });
-  } catch (error) {
-    res.send({
-      success: true,
-      message: "Error",
-      error,
-    });
-  }
-};
+const createMango = catchAsync(async (req: Request, res: Response) => {
+  const data = await MangoServices.createMangoIntoDB(req.body);
+  res.send({
+    success: true,
+    message: "Mango Created Successfully",
+    data,
+  });
+});
+
+const getMangos = catchAsync(async (req: Request, res: Response) => {
+  const data = await Mango.find();
+  
+  res.send({
+    success: true,
+    message: "Mango getting Successfully",
+    data,
+  });
+});
 
 const getMangoById = async (req: Request, res: Response) => {
   try {
     const mangoId = req.params.mangoId;
-    const data = await MangoServices.getMangoByIdFromDB(mangoId)
+    const data = await MangoServices.getMangoByIdFromDB(mangoId);
+
     res.send({
       success: true,
       message: "Mango getting Successfully",
