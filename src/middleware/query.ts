8c321f1473d87e16ae0ad89f276ea48bd1d5ac9d
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-07-24 13:48:31 +0600

    working with search and filter

--- src/middleware/query.ts
+++ src/middleware/query.ts
@@ -1,15 +1,44 @@
 import { NextFunction, Request, Response } from "express";
+import { FilterQuery, Model } from "mongoose";
 
-export const queryBuilder = () => {
- return async (req: Request, res: Response, next: NextFunction) => {
+export const queryBuilder = <T>(model: Model<T>, searchFields: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     try {
-        
-        const queryParam = {...req.query}
-        console.log(queryParam);
+      const queryParams = { ...req.query };
+      const mongoQuery: FilterQuery<T> = {};
 
-        next();
-    } catch (error) {
-        
-    }
+
+      /// Search Operation 
+      const searchTerm = queryParams.searchTerm as string;
+
+      if (searchTerm) {
+        mongoQuery.$or = searchFields.map(
+          (field) =>
+            ({
+              [field]: { $regex: searchTerm, $options: "i" },
+            } as FilterQuery<T>)
+        );
+      }
+
+      const excludeFields = ["searchTerm", "sort"];
+
+      /// Filter Operation 
+      const filter = excludeFields.forEach(
+        (field) => delete queryParams[field]
+      );
+
+      for (const key in queryParams) {
+        const value = queryParams[key];
+
+        if (typeof value === "string") {
+          (mongoQuery as any)[key] = { $regex: value, $options: "i" };
+        }
+      }
+
+      const query = await model.find(mongoQuery);
+      console.log({ query });
+
+      next();
+    } catch (error) {}
   };
 };
--- src/modules/mango/mango.route.ts
+++ src/modules/mango/mango.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { mangoController } from "./mango.controller";
 import { queryBuilder } from "../../middleware/query";
+import Mango from "./mango.model";
 
 const mangoRoute = Router();
 
@@ -8,6 +9,6 @@ mangoRoute.post("/", mangoController.createMango);
 mangoRoute.get("/:mangoId", mangoController.getMangoById);
 mangoRoute.patch("/:mangoId", mangoController.updateMango);
 mangoRoute.delete("/:mangoId", mangoController.deleteMangoById);
-mangoRoute.get("/", queryBuilder(), mangoController.getMangos);
+mangoRoute.get("/", queryBuilder(Mango, ["season", "origin", "name"]));
 
 export default mangoRoute;
